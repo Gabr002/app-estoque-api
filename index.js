0bda69b69fe0baca07b434f26a5261fb190f639b
Gabriel Dantas <gdantaslopes62@gmail.com>
2025-03-08 01:26:14 -0300

    Update environment settings, dependencies, and documentation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const route  = require("./src/routes/product");
+const database = require("./src/database");
 const app = express();
 
 const port = 3000;
@@ -7,6 +8,13 @@ app.use(express.json());
 
 app.use('/api/v1/product', route);
 
-app.listen(port, () => {
-    console.log(`Server run an port ${port}`);
-})
+database.db
+    .sync({ force: false })
+    .then((_) => {
+        app.listen(port, () => {
+            console.log(`Server run an port ${port}`);
+        })
+    })
+    .catch((e) => {
+        console.log(`Not impossible connect an database: ${e}`);
+    })
